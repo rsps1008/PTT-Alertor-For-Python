4fbc57d3cfc985cdfd172c7726f538c872da965f
rsps1008 <rsps1008@gmail.com>
2024-02-22 09:57:54 +0800

    更新config.json方式更新通知參數

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# ptt_alertor
+# PTT-Alertor-For-Python
 
-PTT Alertor 是一個用於監控 PTT 論壇特定版面的新貼文的 Python 程式。根據預先定義的條件，它可以通過 Line 訊息服務向使用者發送通知。(僅測試python3版本)
+
+PTT Alertor 是一個用於爬蟲 PTT 論壇特定板的新文章的 Python 程式。根據預先定義的條件，它可以通過 Line Notify 訊息服務向使用者發送通知。(僅測試python3版本)
 
 ## 運行方法
 
@@ -10,13 +11,19 @@ PTT Alertor 是一個用於監控 PTT 論壇特定版面的新貼文的 Python �
 pip3 install requests beautifulsoup4
 ```
 
-### 2. 設定 Line Notify Token
-在程式碼中的 `token` 變數中填入 Line Notify Token。
+### 2. 取得 Line Notify Token
+在 **<font color=orange>config.json</font>** 中的 `line_token: token` 變數中填入 Line Notify Token。<br>
+```json
+{ 
+	"line_token":{"token":"填入你的Token"}
+}
+```
+可以到官方網站取得取得token(需使用電腦版網頁): https://notify-bot.line.me/my/
 
-### 3. 配置檔案
-修改 `keyword_dict` 變數來設定要監控的 PTT 版面和相關條件。您可以設定關鍵字、推文數閾值、以及作者等。
+### 3. 雲端空間放置 Config 配置檔
+放置 **<font color=orange>config.json</font>** 到公開的雲端空間中(例如: Google雲端硬碟，並設定權限為公開)，複製分享的網址並取代 **<font color=green>pttAlertor.py</font>** 變數: `file_url_GD` 之中的網址
 
-### 4. 運行程式
+### 5. 運行程式
 在終端機中執行以下指令來運行腳本：
 ```bash
 python3 pttAlertor.py
@@ -27,16 +34,16 @@ python3 pttAlertor.py
 
 ## 配置設定
 
-您可以修改 `keyword_dict` 變數來設定您要監控的不同版面以及相關條件。以下是 `keyword_dict` 的範例：
+你可以修改 **<font color=orange>config.json</font>** 中的參數來設定想要監控的不同板以及相關條件(**關鍵字**、**推文數**與**作者**)。<br>以下是 **<font color=orange>config.json</font>** 的 Json 範例：
 
 ```python
 keyword_dict = {
-    "Lifeismoney": {
-        "key": [["Line", "pay"], ["pchome"], ["point"], ["蝦皮"]],
-        "push": 50
-    },
-    "stock": {
-        "author": ["a26893997", "blueian", "chengwaye", "robertshih", "nuggets", "newconfidenc", "guilty13", "drgon", "s10330076", "tamama000", "test520", "zesonpso"]
-    },
-    # 其他版面設定...
+	"Lifeismoney":  { 
+						"key":[["Line","pay"],["point"],["蝦皮"],["導購"]], 
+						"push":50 
+					}, 
+	"Kaohsiung":    { 
+						"author":["exelop"] 
+					}
+    # 其他通知觸發設定...
 }
--- pttAlertor.py
+++ pttAlertor.py
@@ -1,44 +1,30 @@
+import sys
+sys.stdout.reconfigure(encoding='utf-8')
 import requests, time
 from bs4 import BeautifulSoup
 import re, os
 import traceback
 from datetime import datetime
+import json
 
-os.system("export PYTHONIOENCODING=utf8")
+file_url = "https://filedn.com/XXXXXXXXXX/config.json"
+file_url_GD = "https://drive.google.com/uc?id=1RVbI3V1wJHWSiX6qlfu9PITA9-XXXXXX"
+
+# os.system("export PYTHONIOENCODING=utf8")
 
 token = ''
+token_prefix = ''
 sended = []
 chtype = {"key":"關鍵字","push":"推文數","author":"作者",}
+
 
 doubleCheck_Flag = True
 FIRSTBOOT_CHECK_FLAG = True
 
-keyword_dict =  { \
-                    "Lifeismoney":  { \
-                                        "key":[["Line","pay"],["pchome"],["point"],["蝦皮"]], \
-                                        "push":50 \
-                                    }, \
-                    "Kaohsiung":    { \
-                                        "author":["exelop"] \
-                                    }, \
-                    "stock":        { \
-                                        "author": ["a26893997","blueian","chengwaye","robertshih","nuggets","newconfidenc","guilty13","drgon","s10330076","tamama000","test520","zesonpso"], \
-                                        "push":90 \
-                                    }, \
-                    "bank_service": { \
-                                        "key":[["情報","中信"],["情報","國泰"],["情報","聯邦"]], \
-                                        "push":50 \
-                                    }, \
-                    "creditcard":   { \
-                                        "key":[["情報","聯邦"],["情報","國泰"],["情報","中信"]], \
-                                        "push":50 \
-                                    }, \
-                    "mobilecomm":   { \
-                                        "key":[["magisk"]] \
-                                    } \
-                }
+keyword_dict =  dict()
     
 def lineNotifyMessage(msg):
+    global token
     try:
         headers = {
             "Authorization": "Bearer " + token, 
@@ -68,90 +54,143 @@ def make_line_msg(board,keytype,pushNum,poTitle,poUrl,msg):
         while len(sended)>100:
             sended.pop(0)
 
+def check_notify_config():
+    global keyword_dict
+    response = requests.get(file_url)
+    if response.status_code == 200:
+        keyword_dict_new = json.loads(response.content.decode('utf-8'))
+        if keyword_dict_new != keyword_dict:
+            keyword_dict = keyword_dict_new
+            print("keyword_dict更新:\n",keyword_dict,"\n")
+            if not FIRSTBOOT_CHECK_FLAG:
+                lineNotifyMessage(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "keyword dict 更新成功" )
+    else:
+        print("沒有辦法下載dict檔案")
+        lineNotifyMessage(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " 沒有辦法下載dict檔案 ")
+    response.close()
+    
+def check_notify_config_GD():
+    global keyword_dict
+    response = requests.get(file_url_GD)
+    if response.status_code == 200:
+        keyword_dict_new = json.loads(response.content.decode('utf-8'))
+        if keyword_dict_new != keyword_dict:
+            keyword_dict = keyword_dict_new
+            print("keyword_dict更新:\n",keyword_dict,"\n")
+            if not FIRSTBOOT_CHECK_FLAG:
+                lineNotifyMessage(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + "keyword dict 更新成功" )
+    else:
+        print("沒有辦法下載dict檔案")
+        lineNotifyMessage(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " 沒有辦法下載dict檔案 ")
+    response.close()
+    
 if __name__ == '__main__':
+    check_notify_config_GD()
+    failed_attempts = 0
+    for _ in range(5):
+        try:
+            print("token Get:", keyword_dict["line_token"]["token"])
+            token = keyword_dict["line_token"]["token"]
+            token_prefix = keyword_dict["line_token"]["token"][:5]
+            break
+        except:    
+            print("Warring: Cannot get token from Json")
+            failed_attempts += 1
+            time.sleep(2)
+    if failed_attempts == 5:
+        print("Error: Failed to get token after 5 attempts. Exiting program.")
+        sys.exit()
+    
     try:
-        f = open(os.path.dirname(os.path.abspath(__file__))+'/file_io.txt', 'r')
+        f = open(os.path.dirname(os.path.abspath(__file__))+'/file_io_'+token_prefix+'.txt', 'r')
         sended = f.read().split("\n")
+        print("file_io exists")
         f.close()
     except:
-        print("file_io not exist")
+        print("file_io not exists")
 
     lineNotifyMessage(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " PTT ALERT START")
 
     while True:
         try:
+            check_notify_config_GD()
             for board in keyword_dict:  #keyword_dict[board]
-                r = requests.get("https://www.ptt.cc/bbs/"+ board +"/index.html") #將網頁資料GET下來
-                soup = BeautifulSoup(r.text,"html.parser") #將網頁資料以html.parser
+                if board == "line_token":
+                    if token != keyword_dict[board]["token"]:
+                        token = keyword_dict[board]["token"]
+                        print("token update!!!:", keyword_dict[board]["token"])
+                else:
+                    r = requests.get("https://www.ptt.cc/bbs/"+ board +"/index.html") #將網頁資料GET下來
+                    soup = BeautifulSoup(r.text,"html.parser") #將網頁資料以html.parser
 
-                if FIRSTBOOT_CHECK_FLAG:
-                    try:
-                        check_board_exist = soup.select("div.main-container")[0].select("div.bbs-screen")[0].text
-                        if "404 - Not Found" in check_board_exist:
-                            lineNotifyMessage(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " 沒有此版: " + board)
-                    except:
-                        pass
-                
-                posts = soup.select("div.r-ent") 
-
-                for post in posts:  #for each po文
-                    pushNum = ""
-                    poTitle = ""
-                    poUrl = ""
-                    poAuthor = ""
-                    msg = ""
+                    if FIRSTBOOT_CHECK_FLAG:
+                        try:
+                            check_board_exist = soup.select("div.main-container")[0].select("div.bbs-screen")[0].text
+                            if "404 - Not Found" in check_board_exist:
+                                lineNotifyMessage(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " 沒有此版: " + board)
+                        except:
+                            pass
                     
-                    #author
-                    poAuthor =  post.select("div.meta div.author")[0].text
-                    if poAuthor != "-":
-                        #push numbers
-                        pushNum_len = str(post.select("div.nrec")[0].select("span")).split('>', 1 )
-                        if len(pushNum_len)>1:
-                            pushNum_tmp = pushNum_len[1].replace("</span>]","")
-                            if pushNum_tmp == "爆":
-                                pushNum = "100"
-                            else:
-                                pushNum = pushNum_tmp
-                        else:
-                            pushNum  =  "0"
-                        #push title and url
-                        poTitle = post.select("div.title a")[0].text
-                        poUrl = "https://www.ptt.cc"+post.select("div.title a")[0]["href"]
+                    posts = soup.select("div.r-ent") 
+
+                    for post in posts:  #for each po文
+                        pushNum = ""
+                        poTitle = ""
+                        poUrl = ""
+                        poAuthor = ""
+                        msg = ""
                         
-                        if "公告" not in poTitle and "活動" not in poTitle:
-                            if "key" in keyword_dict[board]:
-                                for key2 in keyword_dict[board]["key"]:
-                                    if all(word.lower() in poTitle.lower() for word in key2):
+                        #author
+                        poAuthor =  post.select("div.meta div.author")[0].text
+                        if poAuthor != "-":
+                            #push numbers
+                            pushNum_len = str(post.select("div.nrec")[0].select("span")).split('>', 1 )
+                            if len(pushNum_len)>1:
+                                pushNum_tmp = pushNum_len[1].replace("</span>]","")
+                                if pushNum_tmp == "爆":
+                                    pushNum = "100"
+                                else:
+                                    pushNum = pushNum_tmp
+                            else:
+                                pushNum  =  "0"
+                            #push title and url
+                            poTitle = post.select("div.title a")[0].text
+                            poUrl = "https://www.ptt.cc"+post.select("div.title a")[0]["href"]
+                            
+                            if "公告" not in poTitle and "活動" not in poTitle:
+                                if "key" in keyword_dict[board]:
+                                    for key2 in keyword_dict[board]["key"]:
+                                        if all(word.lower() in poTitle.lower() for word in key2):
+                                            #print(poAuthor,pushNum,poTitle,poUrl)
+                                            msg = '&'.join(str(e) for e in key2)
+                                            make_line_msg(board,"key",pushNum,poTitle,poUrl,msg)
+                                            pass
+                                if "push" in keyword_dict[board]:
+                                    if "X" not in pushNum:
+                                        if int(pushNum) >= keyword_dict[board]["push"]:
+                                            #print(poAuthor,pushNum,poTitle,poUrl)
+                                            if pushNum == "100":
+                                                pushNum = "爆"
+                                            msg = pushNum
+                                            make_line_msg(board,"push",pushNum,poTitle,poUrl,msg)
+                                            pass
+                                if "author" in keyword_dict[board]:
+                                    if poAuthor in keyword_dict[board]["author"]:
                                         #print(poAuthor,pushNum,poTitle,poUrl)
-                                        msg = '&'.join(str(e) for e in key2)
-                                        make_line_msg(board,"key",pushNum,poTitle,poUrl,msg)
+                                        msg = poAuthor
+                                        make_line_msg(board,"author",pushNum,poTitle,poUrl,msg)
                                         pass
-                            if "push" in keyword_dict[board]:
-                                if "X" not in pushNum:
-                                    if int(pushNum) >= keyword_dict[board]["push"]:
-                                        #print(poAuthor,pushNum,poTitle,poUrl)
-                                        if pushNum == "100":
-                                            pushNum = "爆"
-                                        msg = pushNum
-                                        make_line_msg(board,"push",pushNum,poTitle,poUrl,msg)
-                                        pass
-                            if "author" in keyword_dict[board]:
-                                if poAuthor in keyword_dict[board]["author"]:
-                                    #print(poAuthor,pushNum,poTitle,poUrl)
-                                    msg = poAuthor
-                                    make_line_msg(board,"author",pushNum,poTitle,poUrl,msg)
-                                    pass
-                time.sleep(2)
+                    time.sleep(2)
             #break
             FIRSTBOOT_CHECK_FLAG = False
-            time.sleep(60)
+            time.sleep(90)
         except Exception as e:
             print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "error:", e)
             traceback.print_exc()
             time.sleep(10)
         except KeyboardInterrupt:
             # f.close()
-            f = open(os.path.dirname(os.path.abspath(__file__))+'/file_io.txt', 'w')
+            f = open(os.path.dirname(os.path.abspath(__file__))+'/file_io_'+token_prefix+'.txt', 'w')
             f.write("\n".join(sended))
             f.close()
             lineNotifyMessage(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " PTT ALERT STOP")
