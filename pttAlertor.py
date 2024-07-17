7f1655c2bcd1c822024ad95170ec7188a7418230
rsps1008 <rsps1008@gmail.com>
2024-07-17 11:43:24 +0800

    增加效能與爬第二頁

--- pttAlertor.py
+++ pttAlertor.py
@@ -13,8 +13,7 @@ file_url_GD = "https://drive.google.com/uc?id=1RVbI3V1wJHWSiX6qlfu9PITA9-XXXXXX"
 # os.system("export PYTHONIOENCODING=utf8")
 
 token = ''
-token_prefix = ''
-sended = []
+sended = set()
 chtype = {"key":"關鍵字","push":"推文數","author":"作者",}
 
 
@@ -38,6 +37,7 @@ def lineNotifyMessage(msg):
         print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), " lineNotifyMessage Function Error: ", e)
     
 def make_line_msg(board,keytype,pushNum,poTitle,poUrl,msg):
+    global sended
     #print(board,keytype,pushNum,poTitle,poUrl,msg)
     if pushNum == "100":
         pushNum = "爆"
@@ -50,7 +50,7 @@ def make_line_msg(board,keytype,pushNum,poTitle,poUrl,msg):
         except:
             print("Environment encoding error, please add \"PYTHONIOENCODING=utf-8 python3 pttAlertor.py\" when executing")
         print("-----------------------------")
-        sended.append(poUrl)
+        sended.add(poUrl)
         while len(sended)>100:
             sended.pop(0)
 
@@ -92,9 +92,15 @@ if __name__ == '__main__':
             print("token Get:", keyword_dict["line_token"]["token"])
             token = keyword_dict["line_token"]["token"]
             token_prefix = keyword_dict["line_token"]["token"][:5]
+            if os.path.exists("/tmp") and os.path.isdir("/tmp"):
+                fileIOName = "/tmp" + f'/file_io_{token_prefix}.txt'
+            else:
+                fileIOName = os.path.dirname(os.path.abspath(__file__)) + f'\\file_io_{token_prefix}.txt'
+            print("File_IO 位置: ",fileIOName)
             break
         except:    
             print("Warring: Cannot get token from Json")
+            traceback.print_exc()
             failed_attempts += 1
             time.sleep(2)
     if failed_attempts == 5:
@@ -102,11 +108,10 @@ if __name__ == '__main__':
         sys.exit()
     
     try:
-        f = open(os.path.dirname(os.path.abspath(__file__))+'/file_io_'+token_prefix+'.txt', 'r')
-        sended = f.read().split("\n")
-        print("file_io exists")
-        f.close()
-    except:
+        with open(fileIOName, 'a+') as f:
+            sended = set(f.read().split("\n"))
+            print("file_io exists")
+    except FileNotFoundError:
         print("file_io not exists")
 
     lineNotifyMessage(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " PTT ALERT START")
@@ -120,79 +125,95 @@ if __name__ == '__main__':
                         token = keyword_dict[board]["token"]
                         print("token update!!!:", keyword_dict[board]["token"])
                 else:
+                    url_array = []
+                    url_array.append("https://www.ptt.cc/bbs/"+ board +"/index.html")
                     r = requests.get("https://www.ptt.cc/bbs/"+ board +"/index.html") #將網頁資料GET下來
                     soup = BeautifulSoup(r.text,"html.parser") #將網頁資料以html.parser
-
-                    if FIRSTBOOT_CHECK_FLAG:
-                        try:
-                            check_board_exist = soup.select("div.main-container")[0].select("div.bbs-screen")[0].text
-                            if "404 - Not Found" in check_board_exist:
-                                lineNotifyMessage(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " 沒有此版: " + board)
-                        except:
-                            pass
                     
-                    # 首先找到分隔元素
-                    separator = soup.find('div', class_='r-list-sep')
-
-                    # 使用.previous_siblings迭代器遍歷所有位於分隔線之上的兄弟節點
-                    posts = []
-                    for sibling in separator.previous_siblings:
-                        if sibling.name == 'div' and 'r-ent' in sibling.get('class', []):
-                            posts.append(sibling)
-
-                    # 反轉列表以獲得正確的順序（從最舊到最新）
-                    posts = posts[::-1]
-  
-                    # posts = soup.select("div.r-ent") 
-
-                    for post in posts:  #for each po文
-                        pushNum = ""
-                        poTitle = ""
-                        poUrl = ""
-                        poAuthor = ""
-                        msg = ""
+                    previous_page_link = soup.find('div', id='action-bar-container').find('a', string='‹ 上頁')
+                    
+                    if previous_page_link:
+                        #print(previous_page_link['href'])
+                        url_array.append("https://www.ptt.cc"+ previous_page_link['href'])
+                    print(url_array)
+                    for ur in url_array:
                         
-                        #author
-                        poAuthor =  post.select("div.meta div.author")[0].text
-                        if poAuthor != "-":
-                            #push numbers
-                            pushNum_len = str(post.select("div.nrec")[0].select("span")).split('>', 1 )
-                            if len(pushNum_len)>1:
-                                pushNum_tmp = pushNum_len[1].replace("</span>]","")
-                                if pushNum_tmp == "爆":
-                                    pushNum = "100"
-                                else:
-                                    pushNum = pushNum_tmp
-                            else:
-                                pushNum  =  "0"
-                            #push title and url
-                            poTitle = post.select("div.title a")[0].text
-                            poUrl = "https://www.ptt.cc"+post.select("div.title a")[0]["href"]
+                        r = requests.get(ur) 
+                        soup = BeautifulSoup(r.text,"html.parser")
+                        if FIRSTBOOT_CHECK_FLAG:
+                            try:
+                                check_board_exist = soup.select("div.main-container")[0].select("div.bbs-screen")[0].text
+                                if "404 - Not Found" in check_board_exist:
+                                    lineNotifyMessage(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " 沒有此版: " + board)
+                            except:
+                                pass
+                        
+                        # 首先找到分隔元素
+                        separator = soup.find('div', class_='r-list-sep')
+
+                        posts = []
+                        # 使用.previous_siblings 迭代器遍歷所有位於分隔線之上的兄弟節點
+                        if separator:
+                            for sibling in separator.previous_siblings:
+                                if sibling.name == 'div' and 'r-ent' in sibling.get('class', []):
+                                    posts.append(sibling)
+                        else:
+                            # 如果找不到分隔線，將所有包含 'r-ent' 類別的 div 全部加進去
+                            all_divs = soup.find_all('div', class_='r-ent')
+                            posts.extend(all_divs)
+
+                        # 反轉列表以獲得正確的順序（從最舊到最新）
+                        posts = posts[::-1]
+      
+                        # posts = soup.select("div.r-ent") 
+
+                        for post in posts:  #for each po文
+                            pushNum = ""
+                            poTitle = ""
+                            poUrl = ""
+                            poAuthor = ""
+                            msg = ""
                             
-                            if "公告" not in poTitle and "活動" not in poTitle:
-                                if "key" in keyword_dict[board]:
-                                    for key2 in keyword_dict[board]["key"]:
-                                        if all(word.lower() in poTitle.lower() for word in key2):
+                            #author
+                            poAuthor =  post.select("div.meta div.author")[0].text
+                            if poAuthor != "-": # 已刪文
+                                #push numbers
+                                pushNum_len = str(post.select("div.nrec")[0].select("span")).split('>', 1 )
+                                if len(pushNum_len)>1:
+                                    pushNum_tmp = pushNum_len[1].replace("</span>]","")
+                                    if pushNum_tmp == "爆":
+                                        pushNum = "100"
+                                    else:
+                                        pushNum = pushNum_tmp
+                                else:
+                                    pushNum  =  "0"
+                                #push title and url
+                                poTitle = post.select("div.title a")[0].text
+                                poUrl = "https://www.ptt.cc"+post.select("div.title a")[0]["href"]
+                                if "公告" not in poTitle and "活動" not in poTitle:
+                                    if "key" in keyword_dict[board]:
+                                        for key2 in keyword_dict[board]["key"]:
+                                            if all(word.lower() in poTitle.lower() for word in key2):
+                                                #print(poAuthor,pushNum,poTitle,poUrl)
+                                                msg = '&'.join(str(e) for e in key2)
+                                                make_line_msg(board,"key",pushNum,poTitle,poUrl,msg)
+                                                pass
+                                    if "push" in keyword_dict[board]:
+                                        if "X" not in pushNum:
+                                            if int(pushNum) >= keyword_dict[board]["push"]:
+                                                #print(poAuthor,pushNum,poTitle,poUrl)
+                                                if pushNum == "100":
+                                                    pushNum = "爆"
+                                                msg = pushNum
+                                                make_line_msg(board,"push",pushNum,poTitle,poUrl,msg)
+                                                pass
+                                    if "author" in keyword_dict[board]:
+                                        if poAuthor in keyword_dict[board]["author"]:
                                             #print(poAuthor,pushNum,poTitle,poUrl)
-                                            msg = '&'.join(str(e) for e in key2)
-                                            make_line_msg(board,"key",pushNum,poTitle,poUrl,msg)
+                                            msg = poAuthor
+                                            make_line_msg(board,"author",pushNum,poTitle,poUrl,msg)
                                             pass
-                                if "push" in keyword_dict[board]:
-                                    if "X" not in pushNum:
-                                        if int(pushNum) >= keyword_dict[board]["push"]:
-                                            #print(poAuthor,pushNum,poTitle,poUrl)
-                                            if pushNum == "100":
-                                                pushNum = "爆"
-                                            msg = pushNum
-                                            make_line_msg(board,"push",pushNum,poTitle,poUrl,msg)
-                                            pass
-                                if "author" in keyword_dict[board]:
-                                    if poAuthor in keyword_dict[board]["author"]:
-                                        #print(poAuthor,pushNum,poTitle,poUrl)
-                                        msg = poAuthor
-                                        make_line_msg(board,"author",pushNum,poTitle,poUrl,msg)
-                                        pass
-                    time.sleep(2)
+                        time.sleep(2)
             #break
             FIRSTBOOT_CHECK_FLAG = False
             time.sleep(90)
@@ -201,10 +222,9 @@ if __name__ == '__main__':
             traceback.print_exc()
             time.sleep(10)
         except KeyboardInterrupt:
-            # f.close()
-            f = open(os.path.dirname(os.path.abspath(__file__))+'/file_io_'+token_prefix+'.txt', 'w')
-            f.write("\n".join(sended))
-            f.close()
+            with open(fileIOName, 'w+') as f:
+                for item in sended:
+                    f.write(f"{item}\n")
             lineNotifyMessage(datetime.now().strftime('%Y-%m-%d %H:%M:%S') + " PTT ALERT STOP")
             doubleCheck_Flag = False
             break
